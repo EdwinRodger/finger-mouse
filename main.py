dc180a5655303941a35e73b9b959b726870dad27
EdwinRodger <codevaidik@gmail.com>
2023-12-24 21:12:41 +0530

    made code faster and efficient

--- main.py
+++ main.py
@@ -9,11 +9,16 @@ drawing_utils = mp.solutions.drawing_utils
 
 screen_width, screen_height = pyautogui.size()
 index_y = 0
+frame_height, frame_width = None, None
+thumb_x, thumb_y = None, None
 
 while True:
     _, frame = cap.read()
     frame = cv2.flip(frame, 1)
-    frame_height, frame_width, _ = frame.shape
+    
+    if frame_height is None:
+        frame_height, frame_width, _ = frame.shape
+    
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
     output = hand_detector.process(rgb_frame)
@@ -24,23 +29,25 @@ while True:
             drawing_utils.draw_landmarks(frame, hand)
             landmarks = hand.landmark
 
-            for id, landmark in enumerate(landmarks):
-                x = int(landmark.x * frame_width)
-                y = int(landmark.y * frame_height)
+            landmarks_np = np.array([[landmark.x, landmark.y] for landmark in landmarks])
+            landmarks_np[:, 0] *= frame_width
+            landmarks_np[:, 1] *= frame_height
 
-                if id == 8:
-                    cv2.circle(img=frame, center=(x, y), radius=10, color=(0, 255, 255))
-                    index_x = screen_width / frame_width * x
-                    index_y = screen_height / frame_height * y
-                    pyautogui.moveTo(index_x, index_y)
+            x = landmarks_np[:, 0].astype(int)
+            y = landmarks_np[:, 1].astype(int)
 
-                if id == 4:
-                    cv2.circle(img=frame, center=(x, y), radius=10, color=(0, 255, 255))
-                    thumb_x = screen_width / frame_width * x
-                    thumb_y = screen_height / frame_height * y
-                    print("outside", abs(index_y - thumb_y))
-                    if abs(index_y - thumb_y) < 50:
-                        pyautogui.drag(x, y, button="left")
+            index_x = screen_width / frame_width * x[8]
+            index_y = screen_height / frame_height * y[8]
+            thumb_x = screen_width / frame_width * x[4]
+            thumb_y = screen_height / frame_height * y[4]
+
+            cv2.circle(img=frame, center=(x[8], y[8]), radius=10, color=(0, 255, 255))
+            cv2.circle(img=frame, center=(x[4], y[4]), radius=10, color=(0, 255, 255))
+            
+            if abs(index_y - thumb_y) < 50:
+                pyautogui.drag(x[4], y[4], button="left")
+
+            pyautogui.moveTo(index_x, index_y)
 
     cv2.imshow("Virtual Mouse", frame)
     if cv2.waitKey(1) == ord("q"):
